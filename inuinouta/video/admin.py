1c019ae5e47a9add31c6443c79ea1546816da389
inoshiro <inoshirou@gmail.com>
2020-05-05 18:52:16 +0900

    adminサイトをちょっと改善

--- inuinouta/video/admin.py
+++ inuinouta/video/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
-from .models import Channel, Video
+from .models import Channel, Video, Song
 
-admin.site.register(Channel)
-admin.site.register(Video)
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+
+
+class SongInline(admin.TabularInline):
+    model = Song
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'published_at')
+    inlines = [
+        SongInline,
+    ]
+
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'video')
+
+
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(Song, SongAdmin)

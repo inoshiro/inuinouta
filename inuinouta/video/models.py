1820f586ddd0abce49e8f12490d5a12df1114c10
inoshiro <inoshirou@gmail.com>
2020-05-06 01:42:00 +0900

    登録作業の簡略化のため、APIで動画情報を引っ張ってくるようにした

--- inuinouta/video/models.py
+++ inuinouta/video/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import pyyoutube
+import urllib.parse
 
 
 class Channel(models.Model):
@@ -14,15 +16,16 @@ class Channel(models.Model):
     def __str__(self):
         return self.name
 
+
 YOUTUBE_EMBED_BASE_URL = "https://www.youtube.com/embed/"
 
 class Video(models.Model):
     channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
-    title = models.CharField("タイトル", max_length=100, null=True)
+    title = models.CharField("タイトル", max_length=100, blank=True, null=True)
     url = models.URLField("URL")
     #type 歌動画か歌枠かの種別
     thumbnail = models.CharField("サムネイル", max_length=255, blank=True, null=True)
-    published_at = models.DateTimeField("投稿日時", null=True)
+    published_at = models.DateTimeField("投稿日時", blank=True, null=True)
     created_at = models.DateTimeField("作成日時", auto_now_add=True)
     updated_at = models.DateTimeField("更新日時", auto_now=True)
 
@@ -32,6 +35,18 @@ class Video(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            qs = urllib.parse.urlparse(self.url).query
+            video_id = urllib.parse.parse_qs(qs)['v']
+
+            youtube_api = pyyoutube.Api(api_key='AIzaSyBt9xjPse-_lzQTuqQeQ5wkaE-mqzLVa_A')
+            video_info = youtube_api.get_video_by_id(video_id=video_id)
+
+            self.title = video_info.items[0].snippet.title
+            self.published_at = video_info.items[0].snippet.publishedAt
+        super(Video, self).save(*args, **kwargs)
 
     def embedUrl(self):
         return YOUTUBE_EMBED_BASE_URL + self.url.split("=")[1]

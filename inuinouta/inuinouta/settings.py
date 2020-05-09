d45f9c32d80651fef39ebbd9af516cc43ca4da47
inoshiro <inoshirou@gmail.com>
2020-05-09 16:09:00 +0900

    修正

--- inuinouta/inuinouta/settings.py
+++ inuinouta/inuinouta/settings.py
@@ -139,7 +139,7 @@ except ImportError:
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
-    django_heroku.settings(lodal())
+    django_heroku.settings(locals())
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)

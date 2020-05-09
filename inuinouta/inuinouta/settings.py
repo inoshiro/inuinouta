4e55f2f5b1a39cf17ef92a232be9321f137a05b7
inoshiro <inoshirou@gmail.com>
2020-05-09 15:59:27 +0900

    修正

--- inuinouta/inuinouta/settings.py
+++ inuinouta/inuinouta/settings.py
@@ -142,4 +142,4 @@ if not DEBUG:
     django_heroku.settings(lodal())
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)

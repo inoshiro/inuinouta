1e9deb20d21ff34b3288d20b0873c02c19e86ce1
inoshiro <inoshirou@gmail.com>
2021-01-10 10:48:49 +0900

    開発向けSentryセットアップ

--- inuinouta/inuinouta/settings.py
+++ inuinouta/inuinouta/settings.py
@@ -13,6 +13,19 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://f1d191d25777475fb6eb5d10e87ec851@o497085.ingest.sentry.io/5572684",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 

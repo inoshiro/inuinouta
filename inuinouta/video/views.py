b412abf7b51388f97008171b2e40c8ea979abf76
inoshiro <inoshirou@gmail.com>
2021-01-27 15:41:47 +0000

    youtube_playlist.js対応＆シャッフル再生対応

--- inuinouta/video/views.py
+++ inuinouta/video/views.py
@@ -33,29 +33,32 @@ def all_in_one(request):
     if 'rainbow' in request.GET:
         font_rainbow = True
 
-    json_content = []
+    data_playlist = []
+    data_videos = {}
+    data_songs = {}
     for v in videos:
-        content_video = {
-            "id": v.video_id(),
-            "title": v.title,
-            "songs": []
+        data_videos[v.video_id()] = {
+            "title": v.title
         }
         for s in v.sorted_song_set:
             content_song = {
                 "id": s.id,
+                "video_id": v.video_id(),
                 "title": s.title,
                 "artist": s.artist,
                 "start_at": s.start_at,
                 "end_at": s.end_at
             }
-            content_video["songs"].append(content_song)
-        json_content.append(content_video)
+            data_playlist.append(content_song)
+            data_songs[s.id] = content_song
 
     context = {
         'videos': videos,
         'initial_video': initial_video,
         'initial_song': initial_song,
-        'json_content': json.dumps(json_content, ensure_ascii=False),
+        'json_playlist': json.dumps(data_playlist, ensure_ascii=False),
+        'json_songs': json.dumps(data_songs, ensure_ascii=False),
+        'json_videos': json.dumps(data_videos, ensure_ascii=False),
         'font_rainbow': font_rainbow
     }
 

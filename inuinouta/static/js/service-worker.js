c907bc5507bd54cfaa7f826a74eb1091ac8ad91c
inoshiro <inoshirou@gmail.com>
2021-01-28 16:00:27 +0000

    PWAテスト実装

---
+++ inuinouta/static/js/service-worker.js
@@ -0,0 +1,10 @@
+// service-worker.js
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+});
+
+self.addEventListener('fetch', function(event) {});

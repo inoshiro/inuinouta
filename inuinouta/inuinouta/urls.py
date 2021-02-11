8114d8c96f79b13f1dd872b1c31f881006c6577a
inoshiro <inoshirou@gmail.com>
2021-02-11 09:06:57 +0000

    sentry検証用コードの削除

--- inuinouta/inuinouta/urls.py
+++ inuinouta/inuinouta/urls.py
@@ -17,11 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
 urlpatterns = [
-    path('sentry-debug/', trigger_error),
     path('', include('video.urls')),
     path('admin/', admin.site.urls),
 ]
